37ff841617eed0d271e79f25aca5b55245677fd7
amchappell9 <amchappell9@gmail.com>
2020-12-20 17:19:52 -0600

    Sprucing up Page Not Found component

--- src/routes/NoMatch.js
+++ src/routes/NoMatch.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const NoMatch = () => {
-  return <div>The page you are looking for is not found.</div>;
+  return (
+    <div className="min-main-height flex justify-center items-center">
+      <div className="text-center">
+        <h1 className="text-6xl font-bold">Page Not Found</h1>
+        <p className="text-2xl mt-4">
+          The page you are looking for is not found.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default NoMatch;
