49e31f169df205ae99c20d93041b2f4ce4ae268b
amchappell9 <amchappell9@gmail.com>
2020-12-20 19:49:58 -0600

    Changing default background color

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,7 +3,7 @@ import React from "react";
 const Input = ({ className, ...props }) => {
   return (
     <input
-      className={`${className} text-lg bg-gray-50 border border-gray-200 border-solid outline-none focus:ring-2 focus:ring-red-500 rounded`}
+      className={`${className} text-lg border border-gray-300 border-solid outline-none focus:ring-2 focus:ring-red-500 rounded`}
       {...props}
     />
   );
