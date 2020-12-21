d0eb6b3153021747f383e2ecc433af0b3bfcdf18
amchappell9 <amchappell9@gmail.com>
2020-12-20 19:46:40 -0600

    Adding sign up form

---
+++ src/routes/SignUp/SignUp.js
@@ -0,0 +1,57 @@
+import React from "react";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+
+const SignUp = () => {
+  return (
+    <div className="min-main-height flex justify-center items-center">
+      <div className="block max-w-4xl w-full">
+        <h1 className="text-3xl font-bold text-gray-700">Sign Up</h1>
+        <div className="mt-4 mb-8 px-8 bg-white rounded shadow-lg grid grid-cols-3 gap-8">
+          <div className="pt-8">
+            <h2 className="font-bold text-xl text-gray-600">Profile</h2>
+            <p className="text-gray-500">Enter information about yourself.</p>
+          </div>
+          <div className="col-span-2 py-8">
+            <form>
+              <div className="grid grid-cols-2 gap-4">
+                <div className="">
+                  <label className="block mb-2 font-gray-600">First Name</label>
+                  <Input className="w-full bg-gray-50 py-1 px-4" />
+                </div>
+                <div className="">
+                  <label className="block mb-2 font-gray-600">Last Name</label>
+                  <Input className="w-full bg-gray-50 py-1 px-4" />
+                </div>
+              </div>
+              <div className="mt-4">
+                <label className="block mb-2 font-gray-600">
+                  Email Address
+                </label>
+                <Input className="w-full bg-gray-50 py-1 px-4" />
+              </div>
+              <div className="mt-4">
+                <label className="block mb-2 font-gray-600">Password</label>
+                <Input className="w-full bg-gray-50 py-1 px-4" />
+              </div>
+              <div className="mt-4">
+                <label className="block mb-2 font-gray-600">
+                  Confirm Password
+                </label>
+                <Input className="w-full bg-gray-50 py-1 px-4" />
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="text-right">
+          <Button variant="secondary" className="mr-2">
+            Cancel
+          </Button>
+          <Button>Sign Up</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
