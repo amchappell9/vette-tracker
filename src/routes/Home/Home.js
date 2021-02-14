68a9f7d8892bc01b2fe9842bae496e75872cb0de
amchappell9 <amchappell9@gmail.com>
2021-02-14 15:35:24 -0600

    Homepage style tweaks

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -6,16 +6,16 @@ const Home = () => {
   return (
     <>
       <section className="text-center pt-40 mb-20">
-        <h1 className="text-white text-6xl font-bold pb-4">
+        <h1 className="text-white text-7xl font-bold pb-6">
           Looking for a new Vette?
         </h1>
         <div className="block">
-          <span className="inline-block max-w-lg text-gray-200 text-xl pb-6">
+          <span className="inline-block max-w-xl text-gray-200 text-2xl pb-6">
             <span className="text-red-500 font-bold">Vette Tracker</span> helps
             you track Vette prices over time, helping you find the best deals.
           </span>
         </div>
-        <div className="block mt-2">
+        <div className="block mt-6">
           <Link
             to="/sign-in"
             className="px-8 py-4 font-bold text-2xl shadow-md bg-red-500 hover:bg-red-600 text-white rounded"
@@ -29,7 +29,7 @@ const Home = () => {
         <div className="home-triangle-right border-red-500 inline"></div>
       </section>
       <section className="h-32 bg-white -mt-32"></section>
-      <section className="flex justify-between bg-white px-96 py-32">
+      <section className="flex justify-between bg-white px-40 py-48">
         <FeatureCard
           title="Helps You Do This"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac nisl eget quam pellentesque imperdiet sed eget magna. "
