05ef88b28b00bc70a8fefe8073179627636a1adf
amchappell9 <amchappell9@gmail.com>
2021-04-11 14:30:19 -0500

    Adding back errow icon

--- src/routes/AddVette/AddVette.js
+++ src/routes/AddVette/AddVette.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import { ArrowLeftIcon } from "@heroicons/react/outline";
 import AddVetteForm from "./AddVetteForm";
 import ConfirmationView from "./ConfirmationView";
 import UserInfoContext from "../../contexts/UserInfoContext";
@@ -63,7 +64,10 @@ const AddVette = () => {
       <div className="min-main-height flex justify-center">
         <div className="max-w-4xl w-full -mt-44 mb-8">
           <div className="text-gray-300 hover:underline mb-8">
-            <Link to="vettes">{"<- Back to All Vettes"}</Link>
+            <Link to="vettes">
+              <ArrowLeftIcon className="inline align-text-bottom mr-1 w-5 h-5 " />
+              Back to All Vettes
+            </Link>
           </div>
           <div className="flex justify-between items-center">
             <h1 className="text-white text-3xl font-bold">Add Vettes</h1>
