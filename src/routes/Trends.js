78cac05f0966f0d1ac6bb3eaaab27fb98438dcee
amchappell9 <amchappell9@gmail.com>
2021-11-18 22:03:59 -0500

    Tweaking text styles

--- src/routes/Trends.js
+++ src/routes/Trends.js
@@ -3,7 +3,7 @@ import React from "react";
 const Trends = () => {
   return (
     <div className="px-8 py-16 text-center">
-      <p className="text-4xl text-gray-600 font-bold mb-2">
+      <p className="text-3xl text-gray-600 font-bold mb-2">
         Trends Coming Soon!
       </p>
       <p className="text-2xl text-gray-600">
