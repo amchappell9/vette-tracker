fa329db710f0689b4f6614ae2257e565101002f4
amchappell9 <amchappell9@gmail.com>
2021-11-18 22:18:34 -0500

    Fixing mobile styles on authpage button

--- src/components/layouts/AuthenticatedPage.js
+++ src/components/layouts/AuthenticatedPage.js
@@ -301,13 +301,13 @@ function AuthenticatedPage({
             </div>
           )}
 
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between">
+          <div className="flex flex-col gap-4 sm:flex-row sm:justify-between max-w-7xl sm:mx-auto px-4 sm:px-6 lg:px-8">
             {/* Title + Button */}
             <h1 className="text-3xl font-bold text-white">{titleState}</h1>
             {linkTextState && linkConfigState && (
               <Link
                 to={linkConfigState}
-                className="px-4 py-2 text-white bg-red-500 rounded disabled:opacity-50"
+                className="inline-flex items-center justify-center px-4 py-2 text-white bg-red-500 rounded disabled:opacity-50"
               >
                 {linkIconState}
                 {linkTextState}
