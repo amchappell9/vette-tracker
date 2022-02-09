ad37bff263c8d5b774609a89d836b1a09862ac0d
amchappell9 <amchappell9@gmail.com>
2022-02-08 20:04:38 -0500

    Improving mobile responsiveness of home page

--- src/routes/Home/FeatureCard.js
+++ src/routes/Home/FeatureCard.js
@@ -2,15 +2,17 @@ import React from "react";
 
 const FeatureCard = ({ title, description }) => {
   return (
-    <div className="w-96 bg-gray-300 py-8">
+    <div className="max-w-[400px] flex-1 bg-gray-300 py-8 px-2 sm:px-4">
       <div className="flex justify-center">
-        <div className="h-16 w-16 bg-red-500 rounded-full"></div>
+        <div className="h-16 w-16 rounded-full bg-red-500"></div>
       </div>
-      <div className="text-center mt-4 px-4">
-        <span className="font-bold text-gray-800 text-3xl">{title}</span>
+      <div className="mt-4 px-4 text-center">
+        <span className="text-2xl font-bold text-gray-800 lg:text-3xl">
+          {title}
+        </span>
       </div>
-      <div className="text-center mt-2 px-4">
-        <span className="text-xl text-gray-900 ">{description}</span>
+      <div className="mt-2 px-4 text-center">
+        <span className="text-lg text-gray-900 lg:text-xl ">{description}</span>
       </div>
     </div>
   );
