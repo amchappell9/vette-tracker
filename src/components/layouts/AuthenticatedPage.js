b176feb5b1494e54624121a7d68b72b23d7b4edc
amchappell9 <amchappell9@gmail.com>
2022-02-18 22:32:22 -0500

    Adding logout button

--- src/components/layouts/AuthenticatedPage.js
+++ src/components/layouts/AuthenticatedPage.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import { Link, NavLink } from "react-router-dom";
+import { Menu, Transition } from "@headlessui/react";
 
 import { ReactComponent as Logo } from "../../vetteFlagLogo.svg";
 import navLinks from "../../constants/navLinks";
-import { ArrowLeftIcon, MenuIcon } from "@heroicons/react/outline";
+import { ArrowLeftIcon, MenuIcon, CogIcon } from "@heroicons/react/outline";
 import MobileMenu from "./MobileMenu";
 import Footer from "../../Footer";
 import Card from "../Card";
@@ -16,6 +17,7 @@ const AuthenticatedPage = ({
   linkIcon,
   backLinkText,
   backLinkConfig,
+  handleLogout,
 }) => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
@@ -84,13 +86,14 @@ const AuthenticatedPage = ({
       <MobileMenu
         isOpen={mobileMenuOpen}
         dismiss={() => setMobileMenuOpen(false)}
+        handleLogout={handleLogout}
       />
       {/* Header */}
       <div className="bg-gray-700 px-4 pb-32 sm:px-6 md:px-8">
-        <header className="mx-auto flex max-w-7xl border-b border-gray-600 py-4 pt-6">
+        <header className="mx-auto flex max-w-7xl items-center border-b border-gray-600 py-4 pt-6">
           {/* Logo */}
           <div className="flex-1">
-            <Link to="/" className="flex items-center">
+            <Link to="/" className="flex w-max items-center">
               <Logo />
               <div className="ml-6">
                 <span className="text-xl font-bold text-gray-50">
@@ -117,11 +120,47 @@ const AuthenticatedPage = ({
             ))}
           </nav>
 
-          {/* Mobile Menu Button */}
-          <div className="flex justify-end align-baseline sm:flex-1">
+          {/* Logout / Mobile Menu Button */}
+          <div className="flex justify-end gap-4 sm:flex-1">
+            {/* Desktop Logout Button */}
+            <Menu as="div" className="relative ml-3 hidden lg:block">
+              <div>
+                <Menu.Button className="flex h-10 w-10 max-w-xs items-center justify-center rounded-full bg-gray-600 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                  <span className="sr-only">Open user menu</span>
+                  <CogIcon className="h-6 w-6 text-gray-400" />
+                </Menu.Button>
+              </div>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+              >
+                <Menu.Items className="ring-black absolute right-0 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-opacity-5 focus:outline-none">
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        onClick={handleLogout}
+                        className={`block w-full rounded-md border-2 border-white px-4 py-1 ${
+                          active
+                            ? "border-2 border-gray-900 bg-gray-500 text-white"
+                            : "text-gray-700"
+                        }`}
+                      >
+                        <span className="block -translate-y-px">Logout</span>
+                      </button>
+                    )}
+                  </Menu.Item>
+                </Menu.Items>
+              </Transition>
+            </Menu>
+
             <button
               onClick={() => setMobileMenuOpen(true)}
-              className="-mr-2 inline-flex items-center justify-center rounded-md p-2 text-gray-300 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 lg:hidden"
+              className="-m-2 inline-flex items-center justify-center rounded-md p-2 text-gray-300 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 lg:hidden"
             >
               <span className="sr-only">Open main menu</span>
               <MenuIcon className="block h-6 w-6" aria-hidden="true" />
--- src/components/layouts/MobileMenu.js
+++ src/components/layouts/MobileMenu.js
@@ -1,11 +1,11 @@
 import { Fragment } from "react";
 import { Dialog, Transition } from "@headlessui/react";
-import { XIcon } from "@heroicons/react/outline";
+import { XIcon, CogIcon } from "@heroicons/react/outline";
 import { NavLink } from "react-router-dom";
 
 import navLinks from "../../constants/navLinks";
 
-const MobileMenu = ({ isOpen, dismiss }) => {
+const MobileMenu = ({ isOpen, dismiss, handleLogout }) => {
   return (
     <Transition.Root show={isOpen} as={Fragment}>
       <Dialog
@@ -26,7 +26,7 @@ const MobileMenu = ({ isOpen, dismiss }) => {
             <Dialog.Overlay className="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
           </Transition.Child>
 
-          <div className="fixed inset-y-0 right-0 pl-10 max-w-full flex">
+          <div className="fixed inset-y-0 right-0 flex max-w-full pl-10">
             <Transition.Child
               as={Fragment}
               enter="transform transition ease-in-out duration-500 sm:duration-700"
@@ -37,16 +37,16 @@ const MobileMenu = ({ isOpen, dismiss }) => {
               leaveTo="translate-x-full"
             >
               <div className="w-screen max-w-xs">
-                <div className="h-full flex flex-col py-6 bg-gray-700 shadow-xl overflow-y-scroll">
+                <div className="flex h-full flex-col overflow-y-scroll bg-gray-700 py-6 shadow-xl">
                   <div className="px-6 sm:px-6">
                     <div className="flex items-start justify-between">
                       <Dialog.Title className="text-lg font-medium text-gray-50">
                         Vette Tracker
                       </Dialog.Title>
-                      <div className="ml-3 h-7 flex items-center">
+                      <div className="ml-3 flex h-7 items-center">
                         <button
                           type="button"
-                          className="bg-gray-700 rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white-500"
+                          className="focus:ring-white-500 rounded-md bg-gray-700 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2"
                           onClick={dismiss}
                         >
                           <span className="sr-only">Close panel</span>
@@ -55,22 +55,37 @@ const MobileMenu = ({ isOpen, dismiss }) => {
                       </div>
                     </div>
                   </div>
-                  <div className="mt-8 relative flex-1 px-6 sm:px-6">
-                    <nav className="flex flex-col gap-4">
-                      {navLinks.map((link) => (
-                        <NavLink
-                          key={link.path}
-                          to={link.path}
-                          className="text-gray-50 rounded-md py-2 px-2 -my-2 -ml-2  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white-500"
-                          // Since activeClassName appends to the base className instead of replacing entirely, a custom class is needed to avoid
-                          // conflict with the hover styles.
-                          // activeClassName="bg-red-500 text-white px-3 py-2 rounded-md font-medium hover:bg-red-500"
-                          activeClassName="mobile-nav-active"
-                        >
-                          {link.linkName}
-                        </NavLink>
-                      ))}
-                    </nav>
+                  <div className="relative mt-8 flex-1 px-6 sm:px-6">
+                    <div className="border-b border-gray-500 pb-6">
+                      <nav className="flex flex-col gap-4">
+                        {navLinks.map((link) => (
+                          <NavLink
+                            key={link.path}
+                            to={link.path}
+                            className="focus:ring-white-500 -my-2 -ml-2 rounded-md py-2 px-2 text-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2"
+                            // Since activeClassName appends to the base className instead of replacing entirely, a custom class is needed to avoid
+                            // conflict with the hover styles.
+                            // activeClassName="bg-red-500 text-white px-3 py-2 rounded-md font-medium hover:bg-red-500"
+                            activeClassName="mobile-nav-active"
+                          >
+                            {link.linkName}
+                          </NavLink>
+                        ))}
+                      </nav>
+                    </div>
+                    <div className="py-4">
+                      <ul>
+                        <li>
+                          <button
+                            onClick={handleLogout}
+                            className="-mx-2 flex w-full items-center gap-1 rounded px-2 py-2 text-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2"
+                          >
+                            <CogIcon className="h-6" />
+                            <span className="-translate-y-px">Logout</span>
+                          </button>
+                        </li>
+                      </ul>
+                    </div>
                   </div>
                 </div>
               </div>
