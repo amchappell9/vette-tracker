0029a5ef1371b2cc317faf66dc0cc8dee2954c24
amchappell9 <amchappell9@gmail.com>
2022-02-24 20:24:34 -0500

    Fixing file names in git

---
+++ src/constants/trims.js
@@ -0,0 +1,34 @@
+const trims = [
+  {
+    title: "1LT",
+    features: [
+      "8-inch MyLink Display",
+      "Bose 9 Speaker Sound System",
+      "Driver Information Centre",
+      "Power tilt/telescope steering wheel",
+    ],
+  },
+  {
+    title: "2LT",
+    features: [
+      "Heated & Ventilated Seats",
+      "Power seats",
+      "Heads-up display",
+      "Curb view front camera",
+      "Bose Sound System",
+      "Heated, power outside mirrors",
+      "Frameless inside mirror",
+    ],
+  },
+  {
+    title: "3LT",
+    features: [
+      "Upgraded MyLink Display",
+      "Performance Data Recorder",
+      "Perforated Nappa Leather Seats",
+      "Leather-wrapped interior",
+    ],
+  },
+];
+
+export default trims;
