d8e49c916820e250b179eacd7fd2d3c216917fc7
amchappell9 <amchappell9@gmail.com>
2022-02-28 21:16:51 -0500

    Adding label to checkboxes

--- src/routes/AddVette/PackageCheckbox.js
+++ src/routes/AddVette/PackageCheckbox.js
@@ -1,16 +1,30 @@
 import { useField } from "formik";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 const PackageCheckbox = ({ className, name, title, value, description }) => {
   const [field] = useField({ name: name, type: "checkbox", value: value });
+  const [id] = useState(uuidv4());
 
   return (
-    <div className={`${className} packageCheckbox flex p-4`}>
+    <div
+      className={`${className} packageCheckbox flex items-baseline p-4 focus-within:ring focus-within:ring-red-500`}
+    >
       <div className="pr-4">
-        <input type="checkbox" name={name} value={value} {...field} />
+        <input
+          id={id}
+          type="checkbox"
+          name={name}
+          value={value}
+          {...field}
+          className="h-4 w-4 rounded accent-red-500 focus:ring-red-500"
+        />
       </div>
       <div>
-        <span className="block text-lg font-bold text-gray-800">{title}</span>
-        <span className="block text-gray-600">{description}</span>
+        <label htmlFor={id}>
+          <span className="block text-lg font-bold text-gray-800">{title}</span>
+          <span className="block text-gray-600">{description}</span>
+        </label>
       </div>
     </div>
   );
