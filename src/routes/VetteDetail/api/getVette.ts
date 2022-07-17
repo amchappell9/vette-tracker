c239e24e425938445c074c6d7086bb28ba9e7460
amchappell9 <amchappell9@gmail.com>
2022-07-17 19:37:26 -0400

    Refactored vette detail to use react query

---
+++ src/routes/VetteDetail/api/getVette.ts
@@ -0,0 +1,18 @@
+import { useQuery } from "react-query";
+import { axios } from "../../../lib/axios";
+import { VetteObject } from "../../../types/types";
+
+export const getVette = ({
+  vetteId,
+}: {
+  vetteId: string;
+}): Promise<VetteObject> => {
+  return axios.get(`/vettes/${vetteId}`);
+};
+
+export const useVette = ({ vetteId }: { vetteId: string }) => {
+  return useQuery({
+    queryKey: ["vette", vetteId],
+    queryFn: () => getVette({ vetteId }),
+  });
+};
