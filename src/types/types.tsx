d95fde65b7e35e1e90d5ee80cc668c89250c3b9a
Austin Chappell <austinchappell@Austins-MacBook-Pro.local>
2022-12-18 16:08:20 -0500

    Starting to improve Vette object type

--- src/types/types.tsx
+++ src/types/types.tsx
@@ -1,13 +1,20 @@
+export type Year = "2014" | "2015" | "2016" | "2017" | "2018" | "2019";
+
+export type TransmissionTypes = "Manual" | "Automatic";
+
+export type Packages = "MRC" | "NPP" | "PDR";
+
+// Need to improve this type, but right now it breaks the form
 export interface VetteValues {
-  year: string;
+  year: Year;
   miles: string;
   cost: string;
-  transmissionType: string;
+  transmissionType: TransmissionTypes;
   exteriorColor: string;
   interiorColor: string;
   submodel: string;
   trim: string;
-  packages: string[];
+  packages: Packages[];
   link: string;
 }
 
