64df866f7a01784ea1cb6c0b58ead4dbf20b5976
Austin Chappell <austinchappell@Austins-MacBook-Pro.local>
2022-12-18 12:13:33 -0500

    Adding accessiblity test

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -1,7 +1,14 @@
 import { render } from "@testing-library/react";
+import { axe } from "jest-axe";
 import Footer from "./Footer";
 
 it("renders my name", () => {
   const { getByText } = render(<Footer />);
   getByText(/austin chappell/i);
+});
+
+it("is accessible", async () => {
+  const { container } = render(<Footer />);
+  const results = await axe(container);
+  expect(results).toHaveNoViolations();
 });
