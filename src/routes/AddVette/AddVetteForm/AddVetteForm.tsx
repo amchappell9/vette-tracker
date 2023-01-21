2604f0e8c57c0cb38f6a7dcfb10a039d57582448
Austin Chappell <amchappell9@gmail.com>
2023-01-21 17:31:30 -0500

    Updating index.js to ts

--- src/routes/AddVette/AddVetteForm/AddVetteForm.tsx
+++ src/routes/AddVette/AddVetteForm/AddVetteForm.tsx
@@ -11,7 +11,7 @@ import submodels from "../../../constants/submodels";
 import trims from "../../../constants/trims";
 import packages from "../../../constants/packages";
 import FormInput from "../../../components/forms/FromInput/FormInput";
-import FormSelect from "../../../components/forms/FormSelect/FormSelect";
+import FormSelect from "../../../components/forms/FormSelect";
 import FormRadioGroup from "../../../components/forms/FormRadioGroup/FormRadioGroup";
 import FormCheckboxGroup from "../../../components/forms/FormCheckboxGroup/FormCheckboxGroup";
 import ExteriorColorSelect from "../ExteriorColorSelect/ExteriorColorSelect";
