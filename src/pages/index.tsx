1978e4c8078a54673d14732e80a0067ce6abf54a
Austin Chappell <amchappell9@gmail.com>
2023-02-12 14:41:57 -0500

    Initial conversion to Next.js

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <h1 className="text-lg font-bold text-red-700">Home</h1>
+    </div>
+  );
+}
