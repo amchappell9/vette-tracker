c8d604e67dc257099614e494875b8f7e55d409fe
Austin Chappell <amchappell9@gmail.com>
2023-02-12 16:22:31 -0500

    Updating tailwind config to use new folder layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     colors: {
       red: {
