8aabbc4c40138b6e52518ebc0437df75de02d4bb
Austin Chappell <amchappell9@gmail.com>
2023-03-07 22:03:51 -0500

    Hooking up react query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,12 @@
-import { ReactElement, ReactNode } from "react";
+import React, { ReactElement, ReactNode } from "react";
 import { NextPage } from "next";
 import { AppProps } from "next/app";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 
 import "../styles/global.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 // https://nextjs.org/docs/basic-features/layouts#with-typescript
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
@@ -15,13 +18,17 @@ type AppPropsWithLayout = AppProps & {
 };
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const [queryClient] = React.useState(() => new QueryClient());
   const getLayout = Component.getLayout ?? ((page) => page);
 
   const { user } = pageProps;
 
   return (
-    <UserProvider user={user}>
-      {getLayout(<Component {...pageProps} />)}
-    </UserProvider>
+    <QueryClientProvider client={queryClient}>
+      <UserProvider user={user}>
+        {getLayout(<Component {...pageProps} />)}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </UserProvider>
+    </QueryClientProvider>
   );
 }
