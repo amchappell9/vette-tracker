702001573be701768e8e12750bcfc75a90c233ec
Austin Chappell <amchappell9@gmail.com>
2023-05-29 22:16:35 -0400

    Customizing appearance of sign in and sign up pages

--- pages/sign-in/[[...index]].tsx
+++ pages/sign-in/[[...index]].tsx
@@ -3,7 +3,14 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="flex h-full items-center justify-center">
-      <SignIn />
+      <SignIn
+        appearance={{
+          elements: {
+            formButtonPrimary: "bg-red-500 hover:bg-red-600 active:bg-red-700",
+            footerActionLink: "text-red-500 hover:text-red-600",
+          },
+        }}
+      />
     </div>
   );
 }
