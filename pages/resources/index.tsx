124b0ecd0a2e33b460caafdf1c2a2cbcd848146c
Austin Chappell <amchappell9@gmail.com>
2023-05-31 19:17:39 -0400

    Adding filler resource page

---
+++ pages/resources/index.tsx
@@ -0,0 +1,7 @@
+import AuthenticatedPage from "@/components/layouts/AuthenticatedPage/AuthenticatedPage";
+
+const ResourcesPage = () => {
+  return <AuthenticatedPage title="Resources">Resources!</AuthenticatedPage>;
+};
+
+export default ResourcesPage;
