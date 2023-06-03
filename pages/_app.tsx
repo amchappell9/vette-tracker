ec95dc55b13394b3e4a37169f285762b5f35dba1
Austin Chappell <amchappell9@gmail.com>
2023-06-02 21:17:24 -0400

    Adding Vercel Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement, ReactNode } from "react";
 import { NextPage } from "next";
 import { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "../styles/global.css";
@@ -30,6 +31,7 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
           ></meta>
         </Head>
         {getLayout(<Component {...pageProps} />)}
+        <Analytics />
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </ClerkProvider>
