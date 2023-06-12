92ece10893940c94ff21447e7f5ba320eefa625e
Austin Chappell <amchappell9@gmail.com>
2023-06-11 21:00:44 -0400

    Fixing missing favicons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,30 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en" className="bg-gray-700">
-      <Head />
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicons/favicon-16x16.png"
+        />
+        <meta name="theme-color" content="#000000" />
+        <meta
+          name="description"
+          content="Track the prices of C7 Corvettes to find the best deals"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
