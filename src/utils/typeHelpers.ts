44d1365085f200732a1d61e62d764febc496ba2b
Austin Chappell <amchappell9@gmail.com>
2023-06-12 20:03:39 -0400

    Adding a type helper for overriding component props

--- src/utils/typeHelpers.ts
+++ src/utils/typeHelpers.ts
@@ -6,6 +6,8 @@
  * type ButtonProps = OverrideProps<React.ButtonHTMLAttributes<HTMLButtonElement>, {
  * className?: string;
  * }>;
+ *
+ * https://www.totaltypescript.com/tutorials/react-with-typescript/components/overriding-and-removing-component-props/solution
  */
 export type OverrideProps<T, TOverridden> = Omit<T, keyof TOverridden> &
   TOverridden;
