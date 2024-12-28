190f14ecec4da4707119c487ce9ade3d63bf06bc
Austin Chappell <amchappell9@gmail.com>
2024-12-28 17:27:59 -0500

    Fixing missing code coverage

--- jest.config.ts
+++ jest.config.ts
@@ -28,7 +28,15 @@ const config: Config = {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+  collectCoverageFrom: [
+    "<rootDir>/src/**/*.{js,jsx,ts,tsx}",
+    "!**/*.d.ts",
+    "!**/node_modules/**",
+    "!<rootDir>/out/**",
+    "!<rootDir>/.next/**",
+    "!<rootDir>/*.config.js",
+    "!<rootDir>/coverage/**",
+  ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
